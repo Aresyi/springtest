f441abdd5d71eca6967cf25e387b6a17c0d08437
Administrator <369415359@qq.com>
2019-01-09 15:56:36 +0800

    test new branch

--- src/main/java/com/ydj/springtest/SpringtestApplication.java
+++ src/main/java/com/ydj/springtest/SpringtestApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringtestApplication {
 
 	public static void main(String[] args) {
-
+		System.out.println("init");
 		SpringApplication.run(SpringtestApplication.class, args);
 	}
 }
