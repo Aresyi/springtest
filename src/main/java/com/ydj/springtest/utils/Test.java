36ecadc3030ef218b65c3c5704196b3a0dd95d6c
Administrator <369415359@qq.com>
2019-01-14 16:35:57 +0800

    Merge branch 'master' of http://code.woshipm.com/supporter/springtest
    # Conflicts:
    #	src/main/java/com/ydj/springtest/controller/HelloController.java

--- src/main/java/com/ydj/springtest/utils/Test.java
+++ src/main/java/com/ydj/springtest/utils/Test.java
@@ -1,7 +1,7 @@
 package com.ydj.springtest.utils;
 
 public class Test {
-	public void test1() {
+	public void test123() {
 		System.out.println("........................................1111122222");
 	}
 }
